454b60547191a824a89d84efeb1f923ad9cfd4bf
ryan-lecours <rlecours@umass.edu>
2018-01-04 23:39:40 -0500

    From top to bottom:
    Updated Navbar - Name/Logo, Desired Pages
    
    Finished Reveal/Hide feature to Work Ethics section
    
    Updated file system to work with CPanel's setup where I will be hosting
    
    Uploaded to CPanel, site is now live but changes need to be made:
    
    TODO:
    Connect Navbar Links to sections of Page
    Create Education section
    Add C# to Experience Section
    Link Email in Contact Me Section

--- client/build/js/scripts.js
+++ client/build/js/scripts.js
@@ -1,3 +1,25 @@
+const THOROTEXT = document.querySelector("#thoroText");
+const THOROLINK = document.querySelector("#thoroLink");
+const TOPTHOROLINK = document.querySelector("#topThoroLink");
+const THOROARROW = document.querySelector("#thoroArrow");
+
+const EFFICIENCYTEXT = document.querySelector("#efficiencyText");
+const EFFICIENCYLINK = document.querySelector("#efficiencyLink");
+const TOPEFFICIENCYLINK = document.querySelector("#topEfficiencyLink");
+const EFFICIENCYARROW = document.querySelector("#efficiencyArrow");
+
+const CREATTEXT = document.querySelector("#creatText");
+const CREATLINK = document.querySelector("#creatLink");
+const TOPCREATLINK = document.querySelector("#topCreatLink");
+const CREATARROW = document.querySelector("#creatArrow");
+
+
+//Communication Constants
+const COMMTEXT = document.querySelector("#commText");
+const COMMLINK = document.querySelector("#commLink");
+const TOPCOMMLINK = document.querySelector("#topCommLink a");
+const COMMARROW  = document.querySelector("#commArrow");
+
 //Leader Constants
 const LEADERTEXT = document.querySelector("#leaderText");
 const LEADERLINK = document.querySelector("#leaderLink a");
@@ -13,22 +35,77 @@ const TOPTEAMLINK = document.querySelector("#topTeamLink a");
 //Toggles
 var leaderToggle = 1;
 var teamToggle = 1;
+var valueToggle = 1;
+var creatToggle = 1;
+var efficiencyToggle = 1
+var thoroToggle = 1;
 
 
 //Initialize Hide
 LEADERTEXT.classList.add("hide");
 TEAMTEXT.classList.add("hide");
+COMMTEXT.classList.add("hide");
+CREATTEXT.classList.add("hide");
+EFFICIENCYTEXT.classList.add("hide");
+THOROTEXT.classList.add("hide");
 
 //Functions//////////
+function thoroReveal(e, current) {
+  e.preventDefault();
+  if (thoroToggle == 1) {
+    THOROARROW.src = 'client/build/img/dropup.png';
+    thoroToggle = 2;
+  } else {
+    THOROARROW.src = 'client/build/img/dropdown.png';
+    thoroToggle = 1;
+  }
+  THOROTEXT.classList.toggle("hide");
+}
+
+function efficiencyReveal(e, current) {
+  e.preventDefault();
+  if (efficiencyToggle == 1) {
+    EFFICIENCYARROW.src = 'client/build/img/dropup.png';
+    efficiencyToggle = 2;
+  } else {
+    EFFICIENCYARROW.src = 'client/build/img/dropdown.png';
+    efficiencyToggle = 1;
+  }
+  EFFICIENCYTEXT.classList.toggle("hide");
+}
+
+function creatReveal(e, current) {
+  e.preventDefault();
+  if (creatToggle == 1) {
+    CREATARROW.src = 'client/build/img/dropup.png';
+    creatToggle = 2;
+  } else {
+    CREATARROW.src = 'client/build/img/dropdown.png';
+    creatToggle = 1;
+  }
+  CREATTEXT.classList.toggle("hide");
+}
+
+function commReveal(e, current) {
+  e.preventDefault();
+  if (valueToggle == 1) {
+    COMMARROW.src = 'client/build/img/dropup.png';
+    valueToggle = 2;
+  } else {
+    COMMARROW.src = 'client/build/img/dropdown.png';
+    valueToggle = 1;
+  }
+  COMMTEXT.classList.toggle("hide");
+}
 
 //Hide and Reveal Leader section
 function leaderReveal(e, current) {
   e.preventDefault();
   if (leaderToggle == 1) {
-    LEADERARROW.src = '../img/dropup.png';
+    LEADERARROW.src = 'client/build/img/dropup.png';
     leaderToggle = 2;
   } else {
-    LEADERARROW.src = '../img/dropdown.png';
+    LEADERARROW.src = 'client/build/img/dropdown.png';
     leaderToggle = 1;
   }
   LEADERTEXT.classList.toggle("hide");
@@ -38,16 +115,46 @@ function leaderReveal(e, current) {
 function teamReveal(e, current) {
   e.preventDefault();
   if (teamToggle == 1) {
-    TEAMARROW.src = '../img/dropup.png';
+    TEAMARROW.src = 'client/build/img/dropup.png';
     teamToggle = 2;
   } else {
-    TEAMARROW.src = '../img/dropdown.png';
+    TEAMARROW.src = 'client/build/img/dropdown.png';
     teamToggle = 1;
   }
   TEAMTEXT.classList.toggle("hide");
 }
 
 //Event Listeners
+THOROLINK.addEventListener('click', function(e) {
+  thoroReveal(e, this);
+}, false);
+TOPTHOROLINK.addEventListener('click', function(e) {
+  thoroReveal(e, this);
+}, false);
+
+
+EFFICIENCYLINK.addEventListener('click', function(e) {
+  efficiencyReveal(e, this);
+}, false);
+
+TOPEFFICIENCYLINK.addEventListener('click', function(e) {
+  efficiencyReveal(e, this);
+}, false);
+
+CREATLINK.addEventListener('click', function(e) {
+  creatReveal(e, this);
+}, false);
+TOPCREATLINK.addEventListener('click', function(e) {
+  creatReveal(e, this);
+}, false);
+
+COMMLINK.addEventListener('click', function(e) {
+  commReveal(e, this);
+}, false);
+TOPCOMMLINK.addEventListener('click', function(e) {
+  commReveal(e, this);
+}, false);
+
 LEADERLINK.addEventListener('click', function(e) {
   leaderReveal(e, this);
 }, false);
@@ -60,3 +167,4 @@ TEAMLINK.addEventListener('click', function(e) {
 TOPTEAMLINK.addEventListener('click', function(e) {
   teamReveal(e, this);
 }, false);
+COMMTEXT
